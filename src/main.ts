4a0b59e2fd264d1a3a714cf3fa7a7ebaffa773f1
Pontus Johansson <johanssonp998@gmail.com>
2026-04-13 11:20:16 +0200

    Lägger till en händelsehanterar till formuläret

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,19 @@ import "./style/main.scss";
 import { TodoList } from "./classes/TodoList";
 
 const todoList = new TodoList();
+
+// Hämtar element från HTML-sidan
+const form = document.getElementById("todoForm") as HTMLFormElement;
+const taskInput = document.getElementById("taskInput") as HTMLInputElement;
+const prioritySelect = document.getElementById("prioritySelect") as HTMLSelectElement;
+const outputList = document.getElementById("outputList") as HTMLUListElement;
+
+// Lägger till event-lyssnare till formuläret
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const task = taskInput.value;
+    const priority = Number(prioritySelect.value);
+
+    console.log("Task: "+ task, ", Prioritet: "  + priority);
+})
